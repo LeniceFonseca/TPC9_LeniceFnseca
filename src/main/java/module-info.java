fc766107941844b99d2c511a217ed44d00770f98
LeniceFonseca <lfonseca@uta.cv>
2023-05-30 16:51:52 -0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tpc9_lenicefnseca {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.tpc9_lenicefnseca to javafx.fxml;
+    exports com.example.tpc9_lenicefnseca;
+}
